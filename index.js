dbbb2928f602d1b8f9a7b665ca18cfbc9c41f607
madraceee <raajanitheesh@gmail.com>
2025-10-18 18:37:32 +0530

    refactored code to use btree

--- index.js
+++ index.js
@@ -10,25 +10,171 @@ class Node {
 		this.values = new Array()
 		this.isLeaf = true
 	}
+
+	insertVal(val) {
+		let insertValPos = -1
+		for (let i = 0; i < this.keys.length; i++) {
+			if (val < this.keys[i]) {
+				if (i < this.values.length) {
+					insertValPos = i
+					break
+				}
+			}
+		}
+		if (val > this.keys[this.keys.length - 1]) {
+			if (this.keys.length < this.values.length) {
+				insertValPos = this.values.length - 1
+			}
+		}
+
+		if (insertValPos !== -1) {
+			if (this.values[insertValPos].keys.length >= degree - 1) {
+				let { mid, leftNode, rightNode } = this.values[insertValPos].splitTree()
+				this.isLeaf = false
+				this.keys.splice(insertValPos, 0, mid)
+				this.values.splice(insertValPos, 1, leftNode, rightNode)
+				if (val < mid) {
+					leftNode = leftNode.insertVal(val)
+				} else {
+					rightNode = rightNode.insertVal(val)
+				}
+			} else {
+				this.values[insertValPos].insertVal(val)
+			}
+		} else {
+			// val cannot be inserted into values
+			let insertPos = this.keys.length
+			for (let i = 0; i < this.keys.length; i++) {
+				if (val < this.keys[i]) {
+					insertPos = i
+					break
+				}
+			}
+			this.keys.splice(insertPos, 0, val)
+		}
+
+
+
+		// this split
+		if (this.keys.length > degree - 1) {
+			let { mid, leftNode, rightNode } = this.splitTree()
+			let node = new Node()
+			node.keys.push(mid)
+			node.values.push(leftNode, rightNode)
+			node.isLeaf = false
+			return node
+		}
+
+		return this
+	}
+
+	// Output: mid, left, right
+	splitTree() {
+		let middle = Math.floor((degree - 1) / 2)
+		let leftNode = new Node()
+		let rightNode = new Node()
+
+		this.keys.forEach((key, index) => {
+			if (index < middle) {
+				leftNode.insertVal(key)
+			} else if (index > middle) {
+				rightNode.insertVal(key)
+			}
+		})
+		this.values.forEach((value, index) => {
+			if (index <= middle) {
+				leftNode.values.push(value)
+			} else {
+				rightNode.values.push(value)
+			}
+		})
+
+		let middleKey = this.keys[middle]
+
+		return {
+			mid: middleKey,
+			leftNode: leftNode,
+			rightNode: rightNode
+		}
+	}
+
 }
 
-var root = new Node()
-function init() {
-	degree = 4;
-	printBtreeOnCanvas(root)
-}
+class BTree {
+	constructor() {
+		this.root = new Node()
+	}
 
-function addNode() {
-	let value = document.getElementById("nodeValue").value
-	value = parseInt(value)
+	addNode() {
+		let value = document.getElementById("nodeValue").value
+		value = parseInt(value)
 
-	root = insertVal(root, value)
-	printBtreeOnCanvas(root)
-}
+		if (value < 0 || value > 999) {
+			alert("Enter values between 0 and 999")
+			return
+		}
+		this.root = this.root.insertVal(value)
+		this.printTree()
+		document.getElementById("nodeValue").value = ""
+	}
 
-function clearTree() {
-	root = new Node()
-	printBtreeOnCanvas(root)
+	clearTree() {
+		this.root = new Node()
+		this.printTree()
+	}
+
+	printTree() {
+		this.printBtreeOnCanvas(this.root)
+	}
+
+	printBtreeOnCanvas(node) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		let queue = new Array()
+		queue.push({ keyPos: null, prevStart: null, node: node })
+
+		let level = 0
+		while (queue.length > 0) {
+			let q = structuredClone(queue)
+			queue.length = 0
+
+			let totalSpace = q.length * (100 + 50)
+			let starting = 500 - totalSpace / 2
+			for (let i = 0; i < q.length; i++) {
+				ctx.strokeRect(starting + (150 * i), 50 + (level * 100), 100, 50)
+				//print Keys
+				q[i].node.keys.forEach((key, j) => {
+					ctx.fillText(key.toString(), starting + (150 * i) + (33 * j) + 5, 50 + (level * 100) + 25)
+					// Draw Box
+					if (j === degree - 2) {
+						return
+					}
+					ctx.beginPath()
+					ctx.moveTo(starting + (150 * i) + (33 * (j + 1)), 50 + (level * 100))
+					ctx.lineTo(starting + (150 * i) + (33 * (j + 1)), 100 + (level * 100))
+					ctx.stroke()
+					ctx.closePath()
+				})
+
+				// Draw line to parent
+				if (q[i].keyPos !== null) {
+					let connectingStart = q[i].prevStart
+					ctx.beginPath()
+					ctx.moveTo(connectingStart + (33 * q[i].keyPos), 50 + 50 + ((level - 1) * 100))
+					ctx.lineTo(starting + 50 + (150 * i), 50 + level * 100)
+					ctx.stroke()
+					ctx.closePath()
+				}
+
+
+				q[i].node.values.forEach((val, j) => {
+					queue.push({ keyPos: j, prevStart: starting + (150 * i), node: val })
+				})
+			}
+
+			level += 1
+		}
+	}
+
 }
 
 function printBTree(root) {
@@ -40,141 +186,10 @@ function printBTree(root) {
 	}
 }
 
-
-function insertVal(root, val) {
-	let insertValPos = -1
-	for (let i = 0; i < root.keys.length; i++) {
-		if (val < root.keys[i]) {
-			if (i < root.values.length) {
-				insertValPos = i
-				break
-			}
-		}
-	}
-	if (val > root.keys[root.keys.length - 1]) {
-		if (root.keys.length < root.values.length) {
-			insertValPos = root.values.length - 1
-		}
-	}
-
-	if (insertValPos !== -1) {
-		if (root.values[insertValPos].keys.length >= degree - 1) {
-			let { mid, leftNode, rightNode } = splitTree(root.values[insertValPos])
-			root.isLeaf = false
-			root.keys.splice(insertValPos, 0, mid)
-			root.values.splice(insertValPos, 1, leftNode, rightNode)
-			if (val < mid) {
-				insertVal(leftNode, val)
-			} else {
-				insertVal(rightNode, val)
-			}
-		} else {
-			insertVal(root.values[insertValPos], val)
-		}
-	} else {
-		// val cannot be inserted into values
-		let insertPos = root.keys.length
-		for (let i = 0; i < root.keys.length; i++) {
-			if (val < root.keys[i]) {
-				insertPos = i
-				break
-			}
-		}
-		root.keys.splice(insertPos, 0, val)
-	}
-
-
-
-	// Root split
-	if (root.keys.length > degree - 1) {
-		let { mid, leftNode, rightNode } = splitTree(root)
-		root = new Node()
-		root.keys.push(mid)
-		root.values.push(leftNode, rightNode)
-		root.isLeaf = false
-		return root
-	}
-
-	return root
+var bTree = new BTree()
+function init() {
+	degree = 4;
+	bTree.printTree();
 }
 
-// Output: mid, left, right
-function splitTree(root) {
-	let middle = Math.floor((degree - 1) / 2)
-	let leftNode = new Node()
-	let rightNode = new Node()
-
-	root.keys.forEach((key, index) => {
-		if (index < middle) {
-			insertVal(leftNode, key)
-		} else if (index > middle) {
-			insertVal(rightNode, key)
-		}
-	})
-	root.values.forEach((value, index) => {
-		if (index <= middle) {
-			leftNode.values.push(value)
-		} else {
-			rightNode.values.push(value)
-		}
-	})
-
-	rootKey = root.keys[middle]
-
-	return {
-		mid: rootKey,
-		leftNode: leftNode,
-		rightNode: rightNode
-	}
-}
-
-function printBtreeOnCanvas(root) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	let queue = new Array()
-	queue.push({ keyPos: null, prevStart: null, node: root })
-
-	let level = 0
-	while (queue.length > 0) {
-		let q = structuredClone(queue)
-		queue.length = 0
-
-		let totalSpace = q.length * (100 + 50)
-		let starting = 500 - totalSpace / 2
-		for (let i = 0; i < q.length; i++) {
-			ctx.strokeRect(starting + (150 * i), 50 + (level * 100), 100, 50)
-			//print Keys
-			q[i].node.keys.forEach((key, j) => {
-				ctx.fillText(key.toString(), starting + (150 * i) + (33 * j) + 5, 50 + (level * 100) + 25)
-				// Draw Box
-				if (j === degree - 2) {
-					return
-				}
-				ctx.beginPath()
-				ctx.moveTo(starting + (150 * i) + (33 * (j + 1)), 50 + (level * 100))
-				ctx.lineTo(starting + (150 * i) + (33 * (j + 1)), 100 + (level * 100))
-				ctx.stroke()
-				ctx.closePath()
-			})
-
-			// Draw line to parent
-			if (q[i].keyPos !== null) {
-				let connectingStart = q[i].prevStart
-				ctx.beginPath()
-				ctx.moveTo(connectingStart + (33 * q[i].keyPos), 50 + 50 + ((level - 1) * 100))
-				ctx.lineTo(starting + 50 + (150 * i), 50 + level * 100)
-				ctx.stroke()
-				ctx.closePath()
-			}
-
-
-			q[i].node.values.forEach((val, j) => {
-				queue.push({ keyPos: j, prevStart: starting + (150 * i), node: val })
-			})
-		}
-
-		level += 1
-	}
-}
-
-
-init();
+init()
